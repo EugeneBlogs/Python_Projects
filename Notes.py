e5fa83d75a3564781e43a29e9d85713883cc910e
EugeneBlogs <108799072+EugeneBlogs@users.noreply.github.com>
2024-06-30 13:04:38 +0200

    Update Notes.py

--- Notes.py
+++ Notes.py
@@ -18,11 +18,16 @@ print("!!!")
 
 window = Tk()
 window.title("Заметки")
-window.geometry("900x770")
+window.geometry("920x770")
 window.option_add("*tearOff", FALSE)
 window.resizable(width=False, height=False)
 
 name_current_widget = ""
+
+def clear_extra_widgets():
+    for widget in window.winfo_children():
+        if widget not in global_widgets:
+            widget.destroy()
 
 def copy_name():
     name = name_current_widget.replace(".text_", "")
@@ -62,56 +67,56 @@ def delete_note():
     clear_extra_widgets()
     create_notes()
 
-m = Menu(window, tearoff=0)
-m.add_command(label="Скопировать название", command=copy_name)
-m.add_command(label="Скопировать текст", command=copy_text)
-m.add_command(label="Скопировать выделенный текст", command=copy_select_text)
-m.add_command(label="Скопировать цвет", command=copy_color)
-m.add_separator()
-m.add_command(label="Удалить заметку", command=delete_note)
+def delete_notes():
+    if askyesno(title="Удалить все заметки", message=f"Вы действительно хотите удалить все заметки?"):
+        try:
+            db.deldb()
+            messagebox.showwarning("Успешно", f"Заметки удалены!")
+        except:
+            messagebox.showerror("Ошибка", "Что-то пошло не так. Повторите попытку.")
+    clear_extra_widgets()
+    create_notes()
 
-def context_menu(event):
-    global name_current_widget
+def few_notes():
+    count = simpledialog.askstring(title="Количество", prompt="Количество заметок")
     try:
-        name_current_widget = str(event.widget)
-        m.tk_popup(event.x_root, event.y_root)
-    finally:
-        m.grab_release()
-
-def clear_extra_widgets():
-    for widget in window.winfo_children():
-        if widget not in global_widgets:
-            widget.destroy()
-
-def new_note():
-    if len(db.getall()) < 20:
-        color = str(colorchooser.askcolor(initialcolor="white"))
-        color = color[color.find("'") + len("'"):].split()[0].replace("'", "").replace(")", "")
-        color_to_text = "[({" + color + "})]"
-        name = simpledialog.askstring(title="Название", prompt="Название заметки")
-        all_notes = str(db.getall())
-        all_notes = all_notes.replace("dict_keys([", "").replace("])", "").replace("'", "")
-        notes = all_notes.split(", ")
-        if name == "":
-            messagebox.showerror("Ошибка", "Название не может быть пустым.")
-        elif name in notes:
-            messagebox.showerror("Ошибка", "Заметка с таким названем уже существует.")
-        elif "," in name:
-            messagebox.showerror("Ошибка", "Нельзя использовать данный символ в названии.")
-        else:
-            if name != None:
-                text = simpledialog.askstring(title="Текст", prompt="Текст заметки")
-                result_text = f"{color_to_text} {text}"
-                if text != None:
-                    try:
-                        db.set(name, result_text)
-                        messagebox.showinfo("Успешно", f"Заметка \"{name}\" с текстом \"{text}\" успешно создана!")
-                        clear_extra_widgets()
-                        create_notes()
-                    except:
-                        messagebox.showerror("Ошибка", "Что-то пошло не так. Повторите попытку.")
+        count = int(count)
+    except:
+        count = 0
+    if 1 <= count <= 20:
+        new_note(count)
     else:
-        messagebox.showerror("Ошибка", "Нельзя превысить лимит заметок. Максимальное количество - 20.")
+        messagebox.showerror("Ошибка", "Недопустимое число. Число должно быть в диапозоне от 1 до 20.")
+def new_note(repeat = 1):
+    for i in range(repeat):
+        if len(db.getall()) < 20:
+            color = str(colorchooser.askcolor(initialcolor="white"))
+            color = color[color.find("'") + len("'"):].split()[0].replace("'", "").replace(")", "")
+            color_to_text = "[({" + color + "})]"
+            name = simpledialog.askstring(title="Название", prompt="Название заметки")
+            all_notes = str(db.getall())
+            all_notes = all_notes.replace("dict_keys([", "").replace("])", "").replace("'", "")
+            notes = all_notes.split(", ")
+            if name == "":
+                messagebox.showerror("Ошибка", "Название не может быть пустым.")
+            elif name in notes:
+                messagebox.showerror("Ошибка", "Заметка с таким названем уже существует.")
+            elif "," in name:
+                messagebox.showerror("Ошибка", "Нельзя использовать данный символ в названии.")
+            else:
+                if name != None:
+                    text = simpledialog.askstring(title="Текст", prompt="Текст заметки")
+                    result_text = f"{color_to_text} {text}"
+                    if text != None:
+                        try:
+                            db.set(name, result_text)
+                            messagebox.showinfo("Успешно", f"Заметка \"{name}\" с текстом \"{text}\" успешно создана!")
+                            clear_extra_widgets()
+                            create_notes()
+                        except:
+                            messagebox.showerror("Ошибка", "Что-то пошло не так. Повторите попытку.")
+        else:
+            messagebox.showerror("Ошибка", "Нельзя превысить лимит заметок. Максимальное количество - 20.")
 
 def create_notes():
     try:
@@ -151,21 +156,31 @@ def create_notes():
     except:
         print("Созданных заметок ещё нет.")
 
-def delete_notes():
-    if askyesno(title="Удалить все заметки", message=f"Вы действительно хотите удалить все заметки?"):
-        try:
-            db.deldb()
-            messagebox.showwarning("Успешно", f"Заметки удалены!")
-        except:
-            messagebox.showerror("Ошибка", "Что-то пошло не так. Повторите попытку.")
-    clear_extra_widgets()
-    create_notes()
+m = Menu(window, tearoff=0)
+m.add_command(label="Скопировать название", command=copy_name)
+m.add_command(label="Скопировать текст", command=copy_text)
+m.add_command(label="Скопировать выделенный текст", command=copy_select_text)
+m.add_command(label="Скопировать цвет", command=copy_color)
+m.add_separator()
+m.add_command(label="Удалить заметку", command=delete_note)
+m.add_command(label="Удалить все заметки", command=delete_notes)
 
-main_menu = Menu()
-main_menu.add_cascade(label="Создать заметку", command=new_note)
-main_menu.add_cascade(label="Удалить все заметки", command=delete_notes)
+c = Menu(window, tearoff=0)
+c.add_command(label="Создать заметку", command=new_note)
+c.add_command(label="Создать несколько заметок", command=few_notes)
 
-window.config(menu=main_menu)
+def context_menu(event):
+    global name_current_widget
+    try:
+        name_current_widget = str(event.widget)
+        if name_current_widget == ".":
+            c.tk_popup(event.x_root, event.y_root)
+        else:
+            m.tk_popup(event.x_root, event.y_root)
+    finally:
+        m.grab_release()
+
+window.bind("<Button-3>", context_menu)
 
 global_widgets = window.winfo_children()
 
