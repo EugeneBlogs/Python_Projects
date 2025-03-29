f7b2cbfc2b41209e569bd516fe994faba1d11ea1
EugeneBlogs <108799072+EugeneBlogs@users.noreply.github.com>
2025-03-30 00:02:45 +0200

    Update QR Codes.py

--- QR Codes.py
+++ QR Codes.py
@@ -3,16 +3,12 @@
 Для распознования QR-кодов используется библиотека "opencv-python".
 '''
 
-print("!!!")
-print("В консоль будут выводится отчёты об ошибках. Чтобы консоль не открывалась - сохраните файл в формате '.pyw'.")
-print("!!!")
-
 from tkinter import *
 from tkinter import messagebox, simpledialog, filedialog
 
+import cv2
 import numpy as np
 import qrcode
-import cv2
 
 window = Tk()
 window.title("QR-коды")
@@ -20,19 +16,25 @@ window.geometry("230x50")
 window.option_add("*tearOff", FALSE)
 window.resizable(width=False, height=False)
 
+print("!!!")
+print("В консоль будут выводится отчёты об ошибках. Чтобы консоль не открывалась - сохраните файл в формате '.pyw'.")
+print("!!!")
+
+
 def generate():
     root = Tk()
     root.withdraw()
     text = simpledialog.askstring(title="Текст", prompt="Текст QR-кода")
-    if text != None:
+    if text:
         name = simpledialog.askstring(title="Название файла", prompt="Название файла")
-    if name != None:
+    if name:
         img = qrcode.make(text)
         path = filedialog.askdirectory(title="Открыть папку", initialdir="/")
         if path:
             resilt_path = f"{path}/{name}.png"
             img.save(resilt_path)
             messagebox.showinfo("Успешно", f"QR-код с текстом \"{text}\" сохранён по адресу \"{resilt_path}\".")
+
 
 def recognize():
     file_path = filedialog.askopenfilename()
@@ -45,32 +47,38 @@ def recognize():
         data, bbox, straight_qrcode = detector.detectAndDecode(img)
         if bbox is not None: messagebox.showinfo("Расшифровка", data)
 
+
 def on_enter(e):
     e.widget['background'] = '#ffa500'
+
+
 def on_leave_gen(e):
     e.widget['background'] = '#ff0000'
+
+
 def on_leave_rec(e):
     e.widget['background'] = '#ffff00'
 
+
 gen_btn = Button(
-        window,
-        text="Сгенерировать",
-        cursor="hand2",
-        foreground="#000000",
-        background="#ff0000",
-        command=lambda: generate()
+    window,
+    text="Сгенерировать",
+    cursor="hand2",
+    foreground="#000000",
+    background="#ff0000",
+    command=lambda: generate()
 )
 gen_btn.grid(row=1, column=1, padx=15, pady=10)
 gen_btn.bind("<Enter>", on_enter)
 gen_btn.bind("<Leave>", on_leave_gen)
 
 rec_btn = Button(
-        window,
-        text="Распознать",
-        cursor="hand2",
-        foreground="#000000",
-        background="#ffff00",
-        command=lambda: recognize()
+    window,
+    text="Распознать",
+    cursor="hand2",
+    foreground="#000000",
+    background="#ffff00",
+    command=lambda: recognize()
 )
 rec_btn.grid(row=1, column=2, padx=15, pady=10)
 rec_btn.bind("<Enter>", on_enter)
